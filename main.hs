83936471a7fa9807941f89a1f303ea83811c8a09
Sunil S Nandihalli <sunil.nandihalli@gmail.com>
2011-08-11 15:18:21 +0530

    compile error..

--- main.hs
+++ main.hs
@@ -1,5 +1,7 @@
 module Main where
-import Data.List as L
+import qualified Data.List as L
+import qualified Data.HashTable as H
+import qualified Data.Map as M
 
 stringToInteger::String->Integer
 stringToInteger str = read str
@@ -19,6 +21,20 @@ cost locs from= sum $ map (\x->dist from x) locs
 solve::Integral a=>a->[(a,a)]->a
 solve n locs = minimum (map (cost locs) locs)
 
+
+plotAsString::(Integral a)=>[(a,a)]->a->String
+plotAsString locs ans = let xs = map fst locs
+                            ys = map snd locs
+                            locsmap = M.fromList (zip locs [0..])
+                            bufsize=10
+                            min' = (-bufsize) . minimum
+                            max' = ((+) bufsize) . maximum
+                            (bmin@(x0,y0),bmax@(x1,y1)) = ((min' xs,min' ys),(max' xs,max' ys))
+                            plotString = foldl (\curStr newRowId -> foldl (\ccStr newColId -> curStr++(case (M.member (newRowId,newColId) locsmap) of
+                                                                                                         Just v -> "." ++ (show v) ++"."
+                                                                                                         Nothing -> "   ")) curStr [y0..y1]) "" [x0..x1] 
+                        in plotString
+
 tuplify2 :: [a] -> (a,a)
 tuplify2 [x,y] = (x,y)
 
@@ -28,10 +44,7 @@ main =
            n = read nstr
            locs = take (fromIntegral n) $ map (tuplify2 . (\x-> map stringToIntegral $ words x)) locPairStrs
            answer = solve n locs
-       print (answer::Integer)
-
-
-
+       print (answer::Int)
 
 {-|
 cljPartition::[a]->b->
