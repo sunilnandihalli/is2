ce7a6b0cd6b4fba165a52b1894f0c131db5634b9
Sunil S Nandihalli <sunil.nandihalli@gmail.com>
2011-08-11 17:11:32 +0530

    ..

--- main.hs
+++ main.hs
@@ -18,22 +18,24 @@ dist (x1,y1) (x2,y2) = max (abs (x1-x2)) (abs (y1-y2))
 cost::Integral a=>[(a,a)]->(a,a)->a
 cost locs from= sum $ map (\x->dist from x) locs
 
-solve::Integral a=>a->[(a,a)]->a
-solve n locs = minimum (map (cost locs) locs)
+solve::Integral a=>a->[(a,a)]->(a,a)
+solve n locs = L.minimumBy (\x y -> (compare (snd x) (snd y))) (zip (map fromIntegral [0..]) (map (cost locs) locs))
 
 
-plotAsString::(Integral a)=>[(a,a)]->a->String
-plotAsString locs ans = let xs = map fst locs
-                            ys = map snd locs
-                            locsmap = M.fromList (zip locs [0..])
-                            bufsize=10
-                            min' = (-bufsize) . minimum
-                            max' = ((+) bufsize) . maximum
-                            (bmin@(x0,y0),bmax@(x1,y1)) = ((min' xs,min' ys),(max' xs,max' ys))
-                            plotString = foldl (\curStr newRowId -> foldl (\ccStr newColId -> curStr++(case (M.member (newRowId,newColId) locsmap) of
-                                                                                                         Just v -> "." ++ (show v) ++"."
-                                                                                                         Nothing -> "   ")) curStr [y0..y1]) "" [x0..x1] 
-                        in plotString
+plotAsString::(Integral a)=>[(a,a)]->(a,a)->String
+plotAsString locs (ans,_) = let xs = map fst locs
+                                ys = map snd locs
+                                locsmap = M.fromList (zip locs [0..])
+                                bufsize=10
+                                min' = ((\x -> x-bufsize) . minimum)
+                                max' = ((\x -> x+bufsize) . maximum)
+                                (bmin@(x0,y0),bmax@(x1,y1)) = ((min' xs,min' ys),(max' xs,max' ys))
+                                plotString = foldl (\curStr newRowId -> (foldl (\ccStr newColId -> (ccStr++(case (M.lookup (newRowId,newColId) locsmap) of
+                                                                                                              (Just (id,_)) -> (if id==ans then ("|" ++ (show id) ++ "|") 
+                                                                                                                                else ("." ++ (show id) ++ "."))
+                                                                                                              (Nothing) -> " . ")))
+                                                                            curStr [y0..y1]) ++ "\n") "" [x0..x1] 
+                            in plotString
 
 tuplify2 :: [a] -> (a,a)
 tuplify2 [x,y] = (x,y)
@@ -44,18 +46,6 @@ main =
            n = read nstr
            locs = take (fromIntegral n) $ map (tuplify2 . (\x-> map stringToIntegral $ words x)) locPairStrs
            answer = solve n locs
-       print (answer::Int)
-
-{-|
-cljPartition::[a]->b->
-
-
-randomProblem::(Int,[(Integer,Integer)])
-randomProblem=
-    do
-      gen<-getStdGen
-      let locs = map tuplify2 $ cljPartition 2 $ randoms gen::[Int]
-      
-|-}
+       putStrLn $ plotAsString (locs::[(Int,Int)]) (answer::(Int,Int))
 
        
