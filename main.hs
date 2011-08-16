9a4a36e323a3c6d484cc458938057426e51aef48
Sunil S Nandihalli <sunil.nandihalli@gmail.com>
2011-08-16 15:09:32 +0530

    ...

--- main.hs
+++ main.hs
@@ -37,8 +37,37 @@ getEdge locsToId p1 p2 = let Just id1 = M.lookup p1 locsToId
 beachFrontIntersects::(Num a,Ord a)=>[(a,a)]->(a,a)->a
 beachFrontIntersects beachFront newNode = fst newNode
 
-expandToAdvance::(Num a,Ord a)=>((M.Map (a,a) (a,a)),a)->a->((M.Map (a,a) (a,a)),a)
-expandToAdvance (rangeToOpenTrapeziaMap,curSweepLoc) newSweepLoc = (rangeToOpenTrapeziaMap,newSweepLoc)
+intersectParabolas::(Fractional a,Num a,Ord a)=>(a,a)->(a,a)->a->(a,a)
+intersectParabolas p1@(x1,y1) p2@(x2,y2) s
+    | x1 > x2 = intersectParabolas p2 p1 s
+    | dy > (s - x1) / 2 = (s-dy, yav)
+    | x1 == x2 = (sxav,yav)
+    | y1 < y2 = (sxav,y2-hsx)
+    | y1 > y2 = (sxav,y2+hsx)
+    | y1 == y2 = error "intersection not defined!"
+  where dy = abs (y2-y1) / 2
+        yav = (y1+y2)/2
+        sxav = (s+x1)/2
+        hsx = (s-x1)/2
+
+mconvert::[((a,a),(a,a))]->[(a,Maybe (a,a),Maybe (a,a))]
+mconvert listOfRangeValPairs = helper Nothing listOfRangeValPairs
+ where helper _ [] = []
+       helper prev (((y1,y2),v):[]) = (y1,prev,Just v):(y2,Just v,Nothing):[]
+       helper prev (((y1,y2),v):restOfRanges) = (y1,prev,Just v):(helper (Just v) restOfRanges)                                               
+
+newYLocation::(Bounded a,Ord a,Fractional a)=>(a,Maybe (a,a),Maybe (a,a))->a->(a,a)
+newYLocation (y,Nothing,_) _= (y,minBound)
+newYLocation (y,_,Nothing) _= (y,maxBound)
+newYLocation (y,(Just p1),(Just p2)) s = (y,snd $ intersectParabolas p1 p2 s)
+
+expandToAdvance::(Fractional a,Bounded a,Num a,Ord a)=>((M.Map (a,a) (a,a)),a)->a->((M.Map (a,a) (a,a)),a)
+expandToAdvance (rangeToOpenTrapeziaMap,s) s' = let lst = mconvert $ M.toAscList rangeToOpenTrapeziaMap
+                                                    y' = M.fromAscList $ map (\x->newYLocation x s') lst
+                                                in (M.mapKeysMonotonic (\(y1,y2) ->let (Just v1) = M.lookup y1 y'  
+                                                                                       (Just v2) = M.lookup y2 y'
+                                                                                   in (v1,v2))
+                                                    rangeToOpenTrapeziaMap, s' )
 
 deleteOutOfRangeTrapezia::(Num a,Ord a)=>(M.Map (a,a) (a,a))->(a,a)->(M.Map (a,a) (a,a))
 deleteOutOfRangeTrapezia originalTrapeziaMap trimRange@(ymin,ymax) = let Just (((ymin0,ymin1),vmin),restMin) = M.minViewWithKey originalTrapeziaMap
@@ -47,14 +76,13 @@ deleteOutOfRangeTrapezia originalTrapeziaMap trimRange@(ymin,ymax) = let Just ((
                                                                         then deleteOutOfRangeTrapezia (if (ymin1>ymin) 
                                                                                                        then M.insert (ymin,ymin1) vmin restMin 
                                                                                                        else restMin) trimRange 
-                                                                        else 
-                                                                            if (ymax1>ymax) 
-                                                                            then deleteOutOfRangeTrapezia (if (ymax0<ymax) 
-                                                                                                           then M.insert (ymax0,ymax) vmax restMax 
-                                                                                                           else restMax) trimRange
-                                                                            else originalTrapeziaMap                 
+                                                                        else if (ymax1>ymax) 
+                                                                             then deleteOutOfRangeTrapezia (if (ymax0<ymax) 
+                                                                                                            then M.insert (ymax0,ymax) vmax restMax 
+                                                                                                            else restMax) trimRange
+                                                                             else originalTrapeziaMap                 
 
-advanceSweepLineTo::(Num a,Ord a)=>((M.Map (a,a) (a,a)),a)->a->((M.Map (a,a) (a,a)),a)
+advanceSweepLineTo::(Fractional a,Bounded a,Num a,Ord a)=>((M.Map (a,a) (a,a)),a)->a->((M.Map (a,a) (a,a)),a)
 advanceSweepLineTo front@(rangeToOpenTrapeziaMap,curSweepLineLocation) newSweepLineLocation = let delta = newSweepLineLocation - curSweepLineLocation
                                                                                                   (expandedTrapezia,_) = expandToAdvance front newSweepLineLocation
                                                                                                   ((ymin,_),_) = M.findMin rangeToOpenTrapeziaMap
@@ -65,12 +93,12 @@ advanceSweepLineTo front@(rangeToOpenTrapeziaMap,curSweepLineLocation) newSweepL
 addNewPointLocatedAtTheFrontToBeachFront::(Num a)=>(((M.Map (a,a) (a,a)),a),[(b,b)])->(a,a)->(((M.Map (a,a) (a,a)),a),[(b,b)])
 addNewPointLocatedAtTheFrontToBeachFront (beachFront,graphEdges) newPoint = (beachFront,graphEdges)
                    
-addNode::(Num a,Ord a,Integral b)=>M.Map (a,a) b->(((M.Map (a,a) (a,a)),a),[(b,b)])->(a,a)->(((M.Map (a,a) (a,a)),a),[(b,b)])
+addNode::(Fractional a,Bounded a,Num a,Ord a,Integral b)=>M.Map (a,a) b->(((M.Map (a,a) (a,a)),a),[(b,b)])->(a,a)->(((M.Map (a,a) (a,a)),a),[(b,b)])
 addNode locsToId (beachFront,graphEdges) newPoint@(x,_) = let newBeachFront = advanceSweepLineTo beachFront x
                                                           in addNewPointLocatedAtTheFrontToBeachFront (newBeachFront,graphEdges) newPoint
 
              
-vornoiGraph::(Num a,Ord a,Integral b)=>[(a,a)]->[(b,b)]
+vornoiGraph::(Fractional a,Bounded a,Num a,Ord a,Integral b)=>[(a,a)]->[(b,b)]
 vornoiGraph locs = let locsToId = M.fromList (zip locs [0..])
                        idToLocs = M.fromList (zip [0..] locs)
                        xSortedLocs@((xmin,_):_) = L.sort locs
