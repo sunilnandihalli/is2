f887e09d4459e39cc405c23f2387a941e0f906bc
Sunil S Nandihalli <sunil.nandihalli@gmail.com>
2011-08-20 14:11:11 +0530

    ...

--- main.hs
+++ main.hs
@@ -2,6 +2,8 @@ module Main where
 import qualified Data.List as L
 import qualified Data.HashTable as H
 import qualified Data.Map as M
+import qualified Data.Ratio as R
+import Data.Ratio
 
 stringToInteger::String->Integer
 stringToInteger str = read str
@@ -12,16 +14,19 @@ stringToInt str = read str
 stringToIntegral::(Read a,Integral a)=>String->a
 stringToIntegral str = read str
 
-dist::Integral a=>(a,a)->(a,a)->a
+stringToRatio::(Read a,Integral a)=>String->Ratio a
+stringToRatio str = read str
+
+dist::(Integral a)=>(Ratio a,Ratio a)->(Ratio a,Ratio a)->Ratio a
 dist (x1,y1) (x2,y2) = max (abs (x1-x2)) (abs (y1-y2))
 
-cost::Integral a=>[(a,a)]->(a,a)->a
+cost::(Integral a)=>[(Ratio a,Ratio a)]->(Ratio a,Ratio a)->Ratio a
 cost locs from= sum $ map (\x->dist from x) locs
 
-solve::Integral a=>a->[(a,a)]->(a,a)
+solve::(Integral a)=>a->[(Ratio a,Ratio a)]->(a,Ratio a)
 solve n locs = L.minimumBy (\x y -> (compare (snd x) (snd y))) (zip (map fromIntegral [0..]) (map (cost locs) locs))
 
-boundingBox::(Num a,Ord a)=>[(a,a)]->((a,a),(a,a))
+boundingBox::(Integral a,Ord a)=>[(Ratio a,Ratio a)]->((Ratio a,Ratio a),(Ratio a,Ratio a))
 boundingBox locs = let bufsize=1
                        xs = map fst locs
                        ys = map snd locs
@@ -29,15 +34,16 @@ boundingBox locs = let bufsize=1
                        max' = ((\x -> x+bufsize) . maximum)
                    in ((min' xs,min' ys),(max' xs,max' ys))
 
-getEdge::(Num a,Ord a,Integral b)=>M.Map (a,a) b->(a,a)->(a,a)->(b,b)
+getEdge::(Integral a,Ord a,Integral b)=>M.Map (Ratio a,Ratio a) b->(Ratio a,Ratio a)->(Ratio a,Ratio a)->(b,b)
 getEdge locsToId p1 p2 = let Just id1 = M.lookup p1 locsToId
                              Just id2 = M.lookup p2 locsToId
                          in (id1,id2)
-
-beachFrontIntersects::(Num a,Ord a)=>[(a,a)]->(a,a)->a
+{-|
+beachFrontIntersects::(Integral a,Ord a)=>[(Ratio a,Ratio a)]->(Ratio a,Ratio a)->a
 beachFrontIntersects beachFront newNode = fst newNode
+|-}
 
-intersectParabolas::(Fractional a,Num a,Ord a)=>(a,a)->(a,a)->a->(a,a)
+intersectParabolas::(Integral a,Ord a)=>(Ratio a,Ratio a)->(Ratio a,Ratio a)->Ratio a->(Ratio a,Ratio a)
 intersectParabolas p1@(x1,y1) p2@(x2,y2) s
     | x1 > x2 = intersectParabolas p2 p1 s
     | dy > (s - x1) / 2 = (s-dy, yav)
@@ -46,9 +52,9 @@ intersectParabolas p1@(x1,y1) p2@(x2,y2) s
     | y1 > y2 = (sxav,y2+hsx)
     | y1 == y2 = error "intersection not defined!"
   where dy = abs (y2-y1) / 2
-        yav = (y1+y2)/2
-        sxav = (s+x1)/2
-        hsx = (s-x1)/2
+        yav = (y1+y2) / 2
+        sxav = (s+x1) / 2
+        hsx = (s-x1) / 2
 
 mconvert::[((a,a),(a,a))]->[(a,Maybe (a,a),Maybe (a,a))]
 mconvert listOfRangeValPairs = helper Nothing listOfRangeValPairs
@@ -56,14 +62,14 @@ mconvert listOfRangeValPairs = helper Nothing listOfRangeValPairs
        helper prev (((y1,y2),v):[]) = (y1,prev,Just v):(y2,Just v,Nothing):[]
        helper prev (((y1,y2),v):restOfRanges) = (y1,prev,Just v):(helper (Just v) restOfRanges)                                               
 
-newYLocation::(Bounded a,Ord a,Fractional a)=>(a,Maybe (a,a),Maybe (a,a))->a->(a,a)
-newYLocation (y,Nothing,_) _= (y,minBound)
-newYLocation (y,_,Nothing) _= (y,maxBound)
+newYLocation::(Ord a,Integral a)=>(Ratio a,Maybe (Ratio a,Ratio a),Maybe (Ratio a,Ratio a))->Ratio a->(Ratio a,Ratio a)
+newYLocation (y,Nothing,Just (x2,y2)) s = (y,y-s)
+newYLocation (y,Just (x1,y1),Nothing) s = (y,y+s)
 newYLocation (y,(Just p1),(Just p2)) s = (y,snd $ intersectParabolas p1 p2 s)
 
-expandToAdvance::(Fractional a,Bounded a,Num a,Ord a)=>((M.Map (a,a) (a,a)),a)->a->((M.Map (a,a) (a,a)),a)
+expandToAdvance::(Integral a,Ord a)=>((M.Map (Ratio a,Ratio a) (Ratio a,Ratio a)),Ratio a)->Ratio a->((M.Map (Ratio a,Ratio a) (Ratio a,Ratio a)),Ratio a)
 expandToAdvance (rangeToOpenTrapeziaMap,s) s' = let lst = mconvert $ M.toAscList rangeToOpenTrapeziaMap
-                                                    y' = M.fromAscList $ map (\x->newYLocation x s') lst
+                                                    y' = M.fromDistinctAscList $ map (\x->newYLocation x s') lst
                                                 in (M.mapKeysMonotonic (\(y1,y2) ->let (Just v1) = M.lookup y1 y'  
                                                                                        (Just v2) = M.lookup y2 y'
                                                                                    in (v1,v2))
@@ -82,7 +88,7 @@ deleteOutOfRangeTrapezia originalTrapeziaMap trimRange@(ymin,ymax) = let Just ((
                                                                                                             else restMax) trimRange
                                                                              else originalTrapeziaMap                 
 
-advanceSweepLineTo::(Fractional a,Bounded a,Num a,Ord a)=>((M.Map (a,a) (a,a)),a)->a->((M.Map (a,a) (a,a)),a)
+advanceSweepLineTo::(Integral a,Ord a)=>((M.Map (Ratio a,Ratio a) (Ratio a,Ratio a)),Ratio a)->Ratio a->((M.Map (Ratio a,Ratio a) (Ratio a,Ratio a)),Ratio a)
 advanceSweepLineTo front@(rangeToOpenTrapeziaMap,curSweepLineLocation) newSweepLineLocation = let delta = newSweepLineLocation - curSweepLineLocation
                                                                                                   (expandedTrapezia,_) = expandToAdvance front newSweepLineLocation
                                                                                                   ((ymin,_),_) = M.findMin rangeToOpenTrapeziaMap
@@ -90,27 +96,35 @@ advanceSweepLineTo front@(rangeToOpenTrapeziaMap,curSweepLineLocation) newSweepL
                                                                                                   trimmedTrapezia =  deleteOutOfRangeTrapezia expandedTrapezia (ymin,ymax)
                                                                                                in (trimmedTrapezia,newSweepLineLocation)
 
-findParabolaX::(Fractional a,Num a,Ord a)=>(a,a)->a->a->a
+findParabolaX::(Integral a,Ord a)=>(Ratio a,Ratio a)->Ratio a->Ratio a->Ratio a
 findParabolaX (x,y) s y' 
     | dy<=hsx = x+hsx
     | y' < y = s-dy
     | y' > y = s+dy 
  where dy = abs(y'-y)
-       hsx = (s-x)/2
+       hsx = (s-x) / 2
+
+data MaskType = Exclude | Intersect | Untouched deriving (Eq,Show)
+data SideType = PP | PM | MM | MP | PassThrough deriving (Eq,Show)
+
+side::(Num a)=>(a,a)->(a,a)->a->SideType
+side (x,y) (x0,y0) s = PassThrough
 
 masks::(Num a)=>((a,a),(a,a))->(a,a)->(Maybe (a,a),Bool)
 masks ((y1,y2),(x,y)) (nx,ny) 
       | ny == y = (Nothing,True)
 
+
 addNewPointLocatedAtTheFrontToBeachFront::(Num a)=>(((M.Map (a,a) (a,a)),a),[(b,b)])->(a,a)->(((M.Map (a,a) (a,a)),a),[(b,b)])
-addNewPointLocatedAtTheFrontToBeachFront (beachFront,graphEdges) newPoint = (beachFront,graphEdges)
+addNewPointLocatedAtTheFrontToBeachFront (beachFront,graphEdges) newPoint@(x,y) = (beachFront,graphEdges)
                    
-addNode::(Fractional a,Bounded a,Num a,Ord a,Integral b)=>M.Map (a,a) b->(((M.Map (a,a) (a,a)),a),[(b,b)])->(a,a)->(((M.Map (a,a) (a,a)),a),[(b,b)])
+addNode::(Integral a,Ord a,Integral b)=>M.Map (Ratio a,Ratio a) b->(((M.Map (Ratio a,Ratio a) (Ratio a,Ratio a)),Ratio a),[(b,b)])
+       ->(Ratio a,Ratio a)->(((M.Map (Ratio a,Ratio a) (Ratio a,Ratio a)),Ratio a),[(b,b)])
 addNode locsToId (beachFront,graphEdges) newPoint@(x,_) = let newBeachFront = advanceSweepLineTo beachFront x
                                                           in addNewPointLocatedAtTheFrontToBeachFront (newBeachFront,graphEdges) newPoint
 
              
-vornoiGraph::(Fractional a,Bounded a,Num a,Ord a,Integral b)=>[(a,a)]->[(b,b)]
+vornoiGraph::(Integral a,Ord a,Integral b)=>[(Ratio a,Ratio a)]->[(b,b)]
 vornoiGraph locs = let locsToId = M.fromList (zip locs [0..])
                        idToLocs = M.fromList (zip [0..] locs)
                        xSortedLocs@((xmin,_):_) = L.sort locs
@@ -118,7 +132,7 @@ vornoiGraph locs = let locsToId = M.fromList (zip locs [0..])
                    in graphEdges
 
 
-plotAsString::(Integral a)=>[(a,a)]->(a,a)->String
+plotAsString::(Integral a)=>[(Ratio a,Ratio a)]->(a,Ratio a)->String
 plotAsString locs (ansPosId,lowestCost) = let ((x0,y0),(x1,y1)) = boundingBox locs
                                               locsmap = M.fromList (zip locs [0..])
                                           in L.foldl' (\curStr newRowId -> 
@@ -137,8 +151,8 @@ main =
     do input<-getContents
        let (nstr:locPairStrs) = lines input
            n = read nstr
-           locs = take (fromIntegral n) $ map (tuplify2 . (\x-> map stringToIntegral $ words x)) locPairStrs
+           locs = take (fromIntegral n) $ map (tuplify2 . (\x-> map stringToRatio $ words x)) locPairStrs
            answer = solve n locs
-       putStrLn $ plotAsString (locs::[(Int,Int)]) (answer::(Int,Int))
+       putStrLn $ plotAsString locs answer
 
        
